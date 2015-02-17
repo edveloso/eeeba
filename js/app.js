a6388ec4d6576eb25497f8b662f579453a4ae473
edveloso <edveloso@gmail.com>
2015-02-17 00:46:24 -0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,56 @@
+var app = angular.module("app",['toaster']);
+app.controller('UserController', ['$scope', '$window', 'toaster', function ($scope, $window, toaster) {
+	$scope.usuario; 
+	$scope.isAdmin = false;	
+
+	$scope.createUser = function(userEmail, userPassword){
+		alert(userEmail + userPassword);
+		var ref = new Firebase("https://shining-heat-6825.firebaseio.com/");
+		ref.createUser({
+			email    : userEmail,
+			password : userPassword
+		}, function(error, userData) {
+			if (error) {
+				console.log("Error creating user:", error);
+			} else {
+				console.log("Successfully created user account with uid:", userData.uid);
+			}
+		});
+
+	}
+
+	$scope.login = function(userEmail, userPassword){
+
+		var ref = new Firebase("https://shining-heat-6825.firebaseio.com/");
+		ref.authWithPassword({
+			email    : userEmail,
+			password : userPassword
+		}, function(error, authData) {
+			if (error) {
+				toaster.pop('error', "Falha", "Email ou senha falhou!");
+				console.log("Login Failed!", error);
+
+			} else {				
+				
+				$scope.$apply(function() {
+   					$scope.usuario = {'name':userEmail};		
+				});				
+				console.log("Authenticated successfully with payload:", authData);
+				toaster.pop('success', "Sucesso", "Bem vindo ao chat!");
+			}
+		});
+
+	}
+
+
+	$scope.logout = function(){
+		var ref = new Firebase("https://shining-heat-6825.firebaseio.com/");
+		ref.onAuth(function(authData){
+			$scope.usuario;
+			$window.location.reload();
+		});	
+	}
+
+
+
+}]);
